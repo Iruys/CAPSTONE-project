e8ff0056aaced25483ac25782e334084ca4eb25f
reyandru <bariatareiandrew@gmail.com>
2024-08-02 12:31:17 +0800

    Carousel

--- Javascript/home.js
+++ Javascript/home.js
@@ -1,3 +1,4 @@
+// nav bar
 document.addEventListener("DOMContentLoaded", function() {
   const hamburger = document.getElementById('hamburger');
   const nav = document.querySelector('.nav');
@@ -12,7 +13,7 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
-
+// carousel
 document.addEventListener("DOMContentLoaded", function() {
   const ddBtn = document.querySelector('.dropDown');
   const ddConts = document.querySelector('.DD-container');
@@ -22,3 +23,29 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
