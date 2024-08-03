f4c288dab0d332bd11d6ac6c4f37f5b028007ba7
reyandru <bariatareiandrew@gmail.com>
2024-08-03 10:25:19 +0800

    Workout plan input 2

--- Javascript/workoutPlan.js
+++ Javascript/workoutPlan.js
@@ -22,35 +22,51 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-document.getElementById('add-activity').addEventListener('click', function() {
-  document.getElementById('inputContainer').classList.remove('hidden');
+
+
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+
+function openModal() {
+    overlay.style.display = 'block';
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.addEventListener('transitionend', () => {
+        overlay.style.display = 'none';
+        modal.classList.remove('show', 'hide');
+        document.body.style.overflow = 'auto';
+    }, { once: true });
+}
+
+overlay.addEventListener('click', closeModal);
+document.querySelector('.close-button').addEventListener('click', closeModal);
+
+
+
+document.getElementById('add-post-button').addEventListener('click', function() {
+const userInput = document.getElementById('userInput').value;
+if (userInput) {
+const newDiv = document.createElement('div');
+newDiv.className = 'outputDiv';
+
+const span = document.createElement('span');
+span.textContent = userInput;
+newDiv.appendChild(span);
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'Delete';
+deleteBtn.className = 'deleteBtn';
+newDiv.appendChild(deleteBtn);
+
+deleteBtn.addEventListener('click', function() {
+  newDiv.remove();
 });
 
-
-document.getElementById('xBtn').addEventListener('click', function() {
-  document.getElementById('inputContainer').classList.add('hidden');
-});
-
-document.getElementById('submitInputBtn').addEventListener('click', function() {
-  const userInput = document.getElementById('userInput').value;
-  if (userInput) {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'outputDiv';
-    
-    const span = document.createElement('span');
-    span.textContent = userInput;
-    newDiv.appendChild(span);
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.className = 'deleteBtn';
-    newDiv.appendChild(deleteBtn);
-
-    deleteBtn.addEventListener('click', function() {
-      newDiv.remove();
-    });
-
-    document.getElementById('outputContainer').appendChild(newDiv);
-    document.getElementById('userInput').value = ''; // Clear the input field
-  }
+document.getElementById('outputContainer').appendChild(newDiv);
+document.getElementById('userInput').value = ''; // Clear the input field
+}
 });
